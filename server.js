dcdc0fb5cdc59c90ba1ae1c65fcb603cae572293
Wera <m.zajakal@evio.pl>
2021-09-04 19:52:29 +0200

    Added !

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = 8080;
 
 const requestListenner = function(req,res){
     res.writeHead(206);
-    res.end('Kocham Poznan');
+    res.end('Kocham Poznan!');
 }
 const server = http.createServer(requestListenner);
 server.listen(port);
